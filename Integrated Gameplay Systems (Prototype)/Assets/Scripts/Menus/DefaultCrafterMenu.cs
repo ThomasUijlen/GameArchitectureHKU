f58bcb37c0c27311301dd01434ec225f56cc1dc8
Marc313 <marc.rw.neeleman@gmail.com>
2022-10-04 21:31:04 +0200

    Added UIList for generated UI and refactored

--- Integrated Gameplay Systems (Prototype)/Assets/Scripts/Menus/DefaultCrafterMenu.cs
+++ Integrated Gameplay Systems (Prototype)/Assets/Scripts/Menus/DefaultCrafterMenu.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class CrafterMenu : Menu
 {
-    private Dictionary<Button, sRecipe> recipeButtons = new Dictionary<Button, sRecipe>();
     // Misschien later heeft elk menu een Canvas met UI
+    private Dictionary<Button, sRecipe> recipeButtons = new Dictionary<Button, sRecipe>();
     private GameManager gameManager;
     private ICrafter crafter;
 
@@ -17,7 +17,6 @@ public class CrafterMenu : Menu
 
     // Layout \\
     private float buttonOffset = 85f;
-    private Vector3 lastButtonPos;
     
 
     public CrafterMenu(GameManager _gameManager, Canvas _menuCanvas, ICrafter _crafter) : base(_gameManager)
@@ -27,7 +26,6 @@ public class CrafterMenu : Menu
         crafter = _crafter;
 
         AddRecipesToScrollView();
-        //menuCanvas = _menuCanvas;
     }
 
     public override void DisableMenu()
@@ -45,58 +43,53 @@ public class CrafterMenu : Menu
         sRecipeList recipeList = crafter.recipes;
 
         GameObject scrollViewContent = GameObject.Find("Content");      // BAD 
-        // Should add an overload for InstantiatePrefab in prefablibrary
-        GameObject recipeButtonPrefab = gameManager.prefabLibrary.GetPrefab("RecipeButton");
+        GameObject recipeButtonPrefab = gameManager.prefabLibrary.GetPrefab("RecipeButton");    // Should add an overload for InstantiatePrefab in prefablibrary
 
         recipeInfo = GameObject.Find("Recipe Info");
         resultNameText = recipeInfo.GetComponentInChildren<Text>();
 
+        UIList buttonList = new UIList(recipeButtonPrefab, scrollViewContent, 85f);
+
         foreach (sRecipe recipe in recipeList.recipes)
         {
-            CreateRecipeButton(scrollViewContent, recipeButtonPrefab, recipe);
+            GameObject recipeButton = buttonList.AddElement();
+            AddRecipeButtonEvents(recipeButton, recipe);
+            DisplayRecipeUI(recipeButton, recipe);
         }
     }
 
-    private void CreateRecipeButton(GameObject _scrollViewContent, GameObject _recipeButtonPrefab, sRecipe _recipe)
+    private void AddRecipeButtonEvents(GameObject _recipeButton, sRecipe _recipe)
     {
-        // Object pool? Probably not but maybe
-        GameObject recipeButton = GameObject.Instantiate(_recipeButtonPrefab, _scrollViewContent.transform);
-
-        // If more texts, I should use tags to distinguish them?
-        Text recipeNameText = recipeButton.GetComponentInChildren<Text>();
-        if (recipeNameText != null)
-        {
-            recipeNameText.text = _recipe.craftingResult.name;
-        }
-
-        Button button = recipeButton.GetComponent<Button>();
+        Button button = _recipeButton.GetComponent<Button>();
         AddButtonCraftEvent(button, _recipe);
 
         recipeButtons.Add(button, _recipe);
+
+        /*// What if I have 1 list of ingredients and use object pooling
+        GameObject ingredientInfo = gameManager.prefabLibrary.GetPrefab("IngredientInfo");
+        UIList ingredientList = new UIList(ingredientInfo, recipeInfo, 50f);*/
 
         // From Unity Documentation: EventTrigger
         EventTrigger buttonTriggers = button.GetComponent<EventTrigger>();
         EventTrigger.Entry hoverEntry = new EventTrigger.Entry();
         hoverEntry.eventID = EventTriggerType.PointerEnter;
-        hoverEntry.callback.AddListener((data) => UpdateIngedientsInfo((PointerEventData) data));
+        hoverEntry.callback.AddListener((data) => UpdateIngedientsInfo((PointerEventData)data));
         buttonTriggers.triggers.Add(hoverEntry);
+    }
 
-        // Positioning
-        if (lastButtonPos != default)
+    private static void DisplayRecipeUI(GameObject _recipeButton, sRecipe _recipe)
+    {
+        // If more texts, I should use tags to distinguish them?
+        Text recipeNameText = _recipeButton.GetComponentInChildren<Text>();
+        if (recipeNameText != null)
         {
-            lastButtonPos = lastButtonPos - Vector3.up * buttonOffset;
-            recipeButton.transform.position = lastButtonPos;
-        }
-        else
-        {
-            lastButtonPos = recipeButton.transform.position;
+            recipeNameText.text = _recipe.craftingResult.name;
         }
     }
 
     private void UpdateIngedientsInfo(PointerEventData data)
     {
         if (data == null) return;
-        //Debug.Log($"HOVERED over {data.pointerEnter.gameObject.name}");
 
         Button button = data.pointerEnter.GetComponent<Button>();
         if (recipeButtons.ContainsKey(button))
@@ -111,5 +104,4 @@ public class CrafterMenu : Menu
     {
         _button.onClick.AddListener(() => crafter.Craft(_recipe));
     }
-
 }
